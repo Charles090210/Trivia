bb3e4fa8e4d9f65161b39c989a32f548b9bd32cf
Charles <zpiao@ascensionacademy.org>
2026-04-29 09:22:34 -0500

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Charles Piao on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
