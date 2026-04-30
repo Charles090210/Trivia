7d3d45e094075405272422547403d61e7ab04db0
Charles <zpiao@ascensionacademy.org>
2026-04-30 09:30:48 -0500

    First Commit

---
+++ Trivia/Components/PrimaryButton.swift
@@ -0,0 +1,27 @@
+//
+//  PrimaryButton.swift
+//  Trivia
+//
+//  Created by Charles Piao on 4/30/26.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var text: String
+    var background: Color = Color("AccentColor")
+    
+    var body: some View {
+        Text(text)
+            .foregroundStyle(.white)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(30)
+            .shadow(radius: 10)
+    }
+}
+
+#Preview {
+    PrimaryButton(text: "Next")
+}
---
+++ Trivia/Views/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Trivia
+//
+//  Created by Charles Piao on 4/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+        .background(Color(red: 0.9843, green: 0.92941, blue: 0.8470588))
+    }
+}
+
+#Preview {
+    ContentView()
+}
